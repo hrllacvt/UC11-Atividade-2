c5750d76a24a90daa75a11ae5ef1a49c3f9f1e49
hrllacvt <88749605+hrllacvt@users.noreply.github.com>
2023-09-24 18:56:38 -0300

    funcionalidade botao cadastro de produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -22,12 +12,27 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
+   
+    
     public void cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES "
+                + "(?, ?, ?) ";
         
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2,produto.getValor());
+            
+            stmt.execute();
+        }
         
-        //conn = new conectaDAO().connectDB();
+        catch (Exception e){
+            System.out.println("Erro ao inserir produto: " + e.getMessage());
+        }
+       
         
-        
+         conn = new conectaDAO().connectDB(); 
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
@@ -39,4 +44,3 @@ public class ProdutosDAO {
     
         
 }
-
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=vavainha23@");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
